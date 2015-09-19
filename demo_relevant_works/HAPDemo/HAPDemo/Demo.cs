365e7460761116aac9924654c79a8e8d98ad5d61
Abhimanbhau Kolte <abhimanbhau@gmail.com>
2015-09-19 22:28:42 +0530

    Some more iterative demo

---
+++ demo_relevant_works/HAPDemo/HAPDemo/Demo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace HAPDemo
+{
+    class Demo
+    {
+        public static void Run()
+        {
+            HtmlWeb docUrl = new HtmlWeb();
+            
+            for (int i = 0; i < 10; ++i)
+            {
+                var doc = docUrl.Load(String.Format("http://gre.magoosh.com/questions/{0}", i));
+
+                var stuff = doc.DocumentNode.SelectNodes("//div[@class='span8 offset0']");
+                if (stuff == null) continue;
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Out.WriteLine("Fetching GRE Questions -> \n\n" + stuff[0].InnerText);
+            }
+        }
+    }
+}
--- demo_relevant_works/HAPDemo/HAPDemo/Program.cs
+++ demo_relevant_works/HAPDemo/HAPDemo/Program.cs
@@ -11,17 +11,7 @@ namespace HAPDemo
     {
         static void Main(string[] args)
         {
-            HtmlDocument doc = new HtmlDocument();
-            HtmlWeb docUrl = new HtmlWeb();
-            doc = docUrl.Load("http://gre.magoosh.com/questions/5");
-
-            var stuff = doc.DocumentNode.SelectNodes("//div[@class='span8 offset0']");
-            foreach (var stuffed in stuff)
-            {
-                Console.WriteLine(stuffed.InnerText);
-            }
-            if (stuff == null) return;
-            Console.WriteLine("This is the shit i fetched -> \n" + stuff[0].InnerText);
+            Demo.Run();
         }
     }
 }
