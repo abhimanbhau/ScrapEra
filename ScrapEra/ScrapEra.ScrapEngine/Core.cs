e7f35821bc9759a586a7dcd0d8b6bfc34a9b4a95
Abhimanbhau Kolte <abhimanbhau@gmail.com>
2015-09-28 21:46:33 +0530

    Init Core Engine boilerplate

---
+++ ScrapEra/ScrapEra.ScrapEngine/Core.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapEra.ScrapEngine
+{
+    public class Core
+    {
+        public static Engine GetInstance()
+        {
+            return new Engine();
+        }
+    }
+}
