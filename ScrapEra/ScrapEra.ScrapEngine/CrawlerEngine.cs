d57e8b6ac7d8d384fb3b60c269dfd9424889f6c6
Abhimanbhau Kolte <abhimanbhau@gmail.com>
2015-09-28 23:13:24 +0530

    Logger implementation complete

--- ScrapEra/ScrapEra.ScrapEngine/CrawlerEngine.cs
+++ ScrapEra/ScrapEra.ScrapEngine/CrawlerEngine.cs
@@ -16,12 +16,32 @@ namespace ScrapEra.ScrapEngine
         public CrawlerEngine(string url)
         {
             Url = url;
+            ScrapLogger.Logger.LogI("Initialized crawler with URL='" + url + "'");
         }
 
         public void StartCrawl()
         {
-            HtmlWeb docUrl = new HtmlWeb();
-            _doc = docUrl.Load(Url);
+            try
+            {
+                ScrapLogger.Logger.LogI("Initialized crawler with URL='" + Url + "'");
+                HtmlWeb docUrl = new HtmlWeb();
+                _doc = docUrl.Load(Url);
+            }
+            catch (Exception e)
+            {
+                ScrapLogger.Logger.LogE("Fatal Error -> " + e.StackTrace + Environment.NewLine
+                    + e.Message);
+            }
+        }
+
+        public List<String> GetAllParagraphText()
+        {
+            return _doc.DocumentNode.SelectNodes("//p").Select(para => para.InnerHtml).ToList();
+        }
+
+        public List<String> GetAllLinks()
+        {
+            return _doc.DocumentNode.SelectNodes("//a").Select(link => link.InnerHtml).ToList();
         }
     }
 }
---
+++ ScrapEra/ScrapEra.ScrapLogger/Logger.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapEra.ScrapLogger
+{
+    public class Logger
+    {
+        #region class fields
+        private static StreamWriter _stream;
+        private static string _fileName;
+        public static List<String> _LogList = new List<string>();
+        private static string _debugSymbol = "[DEBUG]";
+        private static string _infoSymbol = "[INFO]";
+        private static string _errorSymbol = "[ERROR]";
+        #endregion
+
+
+        static Logger()
+        {
+            Instance = new Logger();
+            _fileName = Path.GetTempFileName().Replace(".tmp", ".scrapLog");
+            _stream = new StreamWriter(_fileName) { AutoFlush = true, NewLine = Environment.NewLine };
+        }
+
+        public static Logger Instance
+        {
+            get;
+            private set;
+        }
+
+        public static void LogI(string message)
+        {
+            message = String.Format("{0} {1}-{2} {3}", _infoSymbol,
+                DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), message);
+            _stream.WriteLine();
+        }
+
+        public static void LogE(string message)
+        {
+            message = String.Format("{0} {1}-{2} {3}", _errorSymbol,
+                DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), message);
+            _stream.WriteLine();
+        }
+
+        public static void LogD(string message)
+        {
+            message = String.Format("{0} {1}-{2} {3}", _debugSymbol,
+                DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), message);
+            _stream.WriteLine();
+        }
+
+        ~Logger()
+        {
+            if (_stream != null)
+            {
+                _stream.Flush();
+                _stream.Close();
+            }
+        }
+    }
+}
