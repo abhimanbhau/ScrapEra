9906d9fc975fb430911681e5a1273e990340f94a
Abhimanbhau Kolte <abhimanbhau@gmail.com>
2015-09-28 22:05:01 +0530

    HAP added and some more progress

--- ScrapEra/ScrapEra.ScrapEngine/CrawlerEngine.cs
+++ ScrapEra/ScrapEra.ScrapEngine/CrawlerEngine.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace ScrapEra.ScrapEngine
 {
     public class CrawlerEngine
     {
-        public string url { get; set; }
+        public string Url { get; set; }
+
+        private HtmlDocument _doc;
+
+        public CrawlerEngine(string url)
+        {
+            Url = url;
+        }
+
+        public void StartCrawl()
+        {
+            HtmlWeb docUrl = new HtmlWeb();
+            _doc = docUrl.Load(Url);
+        }
     }
 }
