da140bf52e09b499d7c47fef8306c00c87471d19
Abhimanbhau Kolte <abhimanbhau@gmail.com>
2015-09-29 13:38:41 +0530

    init gui project

---
+++ ScrapEra/ScrapEraGui/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.Windows.Forms;
+
+namespace ScrapEraGui
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
