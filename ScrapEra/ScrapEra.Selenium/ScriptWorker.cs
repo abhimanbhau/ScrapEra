fe4b49821700828964ba28354365b444f3ad1285
Abhimanbhau Kolte <abhimanbhau@gmail.com>
2015-12-11 01:52:48 +0530

    bb

--- ScrapEra/ScrapEra.Selenium/ScriptWorker.cs
+++ ScrapEra/ScrapEra.Selenium/ScriptWorker.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
+using OpenQA.Selenium.IE;
+using ScrapEra.Selenium;
 
 namespace ScrapEra.Selenium
 {
-    public class ScriptWorker
+    public static class ScriptWorker
     {
+        public static void RunScript(ref InternetExplorerDriver driver, string scriptPath)
+        {
+            var scriptContent = File.ReadAllLines(scriptPath);
+            foreach (var line in scriptContent)
+            {
+                if (line.ToUpper().Contains("INIT"))
+                {
+                    driver = SeleniumCoreIE.GetIeDriverInstance(line.Split('')[1].Replace("\"", ""));
+                }
+                else if (line.ToUpper().Contains("NAVIGATE"))
+                {
+                    driver.Navigate().GoToUrl(line.Split(' ')[1].Replace("\"", ""));
+                }
+                else if (line.ToUpper().Contains("TYPE"))
+                {
+                    
+                }
+                else if (line.ToUpper().Contains("CLICK"))
+                {
+                    
+                }
+            }
+        }
+
+        public static void RunScript(ref InternetExplorerDriver driver, IEnumerable<string> scriptContent)
+        {
+
+        }
     }
 }
